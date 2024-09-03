98207e219962e38272c1d3bdcafba4075d1e00b6
Oliver Then <oli171dr@gmail.com>
2024-09-03 19:23:48 -0400

    Use Client Components, set up SQLite DB, adding loading and error pages

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,3 +1,36 @@
+import MealsGrid from '@/components/meals/meals-grid';
+import Link from 'next/link';
+import classes from './page.module.css';
+import { getMeals } from '@/lib/meals';
+import { Suspense } from 'react';
+
+async function Meals() {
+	const meals = await getMeals();
+
+	return <MealsGrid meals={meals} />;
+}
+
 export default function MealsPage() {
-  return <h1>Meals Page</h1>;
+
+  return (
+		<>
+		<header className={classes.header}>
+			<h1>
+				Delicious meals, created{' '} <span className={classes.highlight}>by you</span>
+			</h1>
+			<p>Chose your favorite recipe and cook it yourself. It is easy and fun</p>
+			<p className={classes.cta}>
+				<Link href="/meals/share">
+				Share Your Favorite Recipe
+				</Link>
+			</p>
+		</header>
+		<main className={classes.main}>
+			<Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+				<Meals />
+			</Suspense>
+			
+		</main>
+		</>
+	);
 }
